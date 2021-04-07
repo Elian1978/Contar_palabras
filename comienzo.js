06d416eca224bc8a3538edada73f6b47ff09147a
Elian Rojas <impresosgosen@gmail.com>
2021-04-07 12:49:39 -0600

    Function count letter ready

--- comienzo.js
+++ comienzo.js
@@ -1,23 +1,30 @@
-const div_resultados = document.querySelector('#resultados')
+// const div_resultados = document.querySelector('#palabras')
+// const div_resultados_letras = document.querySelector('#letras')
 const texto = document.querySelector("#texto-entrada").innerText;
-const palabras = texto.split(' ');
-const resultados = {} // Object
+const palabras = texto.split(' ').map(palabra => limpiarTexto(palabra))
+const letras = palabras.join(' ').split('')
+const resultados_palabras = {} 
+const resultados_letras = {}
 
-// debugger;
-palabras.forEach(palabra => {
-    palabra = palabra.replace(',', '').replace('.', '').toLowerCase()
-    if (palabra in resultados) {
-      resultados[palabra] = resultados[palabra] + 1
-    } else {
-      resultados[palabra] = 1
-    }
+function revision(arr, obj){
+  arr.forEach(txt => {
+    obj[txt] = txt in obj ? obj[txt] + 1 : 1
   });
+}
+function limpiarTexto(txt){
+  return txt.replace(',', '').replace('.', '').toLowerCase()
+}
 
-//Object.keys extraer llaves
-Object.keys(resultados).forEach(resultado => {
-    const resultadito = document.createElement('li')
-    resultadito.innerText = resultado + ' ' + resultados[resultado]
-    div_resultados.appendChild(resultadito)
-})
+function imprimir(obj, lugar){
+  Object.keys(obj).forEach(txt => {
+    const li = document.createElement('li')
+    li.innerText = txt + ' ' + obj[txt]
+    document.querySelector(`#${lugar}`).appendChild(li)
+  })
+}
 
-console.log(resultados)
+revision(palabras, resultados_palabras)
+imprimir(resultados_palabras, 'palabras')
+
+revision(letras, resultados_letras)
+imprimir(resultados_letras, 'letras')
